d08c1e8469b05587e73b39d3e9a426398d54a838
hossein <abdollahzadeh.hossein@gmail.com>
2023-08-11 11:38:02 +0330

    Revert "php artisan module:make Brand --api"
    This reverts commit 599be9c1d82ea541789720c9a75d81c4685e9181.

--- Modules/Brand/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('brand::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('brand.name') !!}
-    </p>
-@endsection
