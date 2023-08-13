5e9c90c186003c134927b141865193ed7a73ec20
hossein <abdollahzadeh.hossein@gmail.com>
2023-08-13 08:04:17 +0330

    Return Handler.php file to default directory

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,14 +5,17 @@ namespace App\Exceptions;
 use Error;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\DB;
+use Modules\Common\Traits\ApiResponse;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-
+    use ApiResponse;
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -32,5 +35,46 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            DB::rollBack();
+            return $this->errorResponse($e->getMessage(), 404);
+        }
+
+        if ($e instanceof NotFoundHttpException) {
+            DB::rollBack();
+            return $this->errorResponse($e->getMessage(), 404);
+        }
+
+        if ($e instanceof MethodNotAllowedHttpException) {
+            DB::rollBack();
+            return $this->errorResponse($e->getMessage(), 405);
+        }
+
+        if ($e instanceof Exception) {
+            DB::rollBack();
+            return $this->errorResponse($e->getMessage(), 500);
+        }
+
+        if ($e instanceof Error) {
+            DB::rollBack();
+            return $this->errorResponse($e->getMessage(), 500);
+        }
+
+        if ($e instanceof QueryException) {
+            DB::rollBack();
+            return $this->errorResponse($e->getMessage(), 500);
+        }
+
+        if (config('app.debug')) {
+            DB::rollBack();
+            return parent::render($request, $e);
+        }
+
+        DB::rollBack();
+        return $this->errorResponse($e->getMessage(), 500);
     }
 }
